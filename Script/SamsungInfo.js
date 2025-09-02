b69c294bc6a5918f37bbb838948fe0f14b47247c
EdisonCYY <sakuraflare@outlook.com>
2025-09-02 15:43:28 +0800

    Update

--- Script/SamsungInfo.js
+++ Script/SamsungInfo.js
@@ -72,7 +72,7 @@ $httpClient.get(preLoading, (error, response, data) => {
         let signalInfo = `${signalStrength} (${signalPercent}%)`;
         $done({
                 title: "Samsung 5G Mobile Wi-Fi",
-                content: `📶Signal: ${signalInfo}\n📅Month Usage: ${monthUsage}\n🗓3-Day Usage: ${threeDayUsage}`,
+                content: `📶Signal: ${signalInfo}\n📅Month: ${monthUsage}\n🗓3-Day: ${threeDayUsage}`,
                 icon: "wifi",
                 "icon-color": "#006afeff"
             });
